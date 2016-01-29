a7f2b2a23e39bbf1c90fe982dd6f1e91ec8a7de7
Simone Basso <bassosimone@gmail.com>
2016-01-30 00:48:32 +0100

    Refactor app.js implementation

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 jQuery("load", function () {
+
+    function drawTable(result) { // XXX Very naive impl.
+        var html = "<table>";
+        for (var i = 0; i < result.length; ++i) {
+            html += "<tr><td>";
+            html += result[i][0];
+            html += "</td><td>";
+            html += result[i][1];
+            html += "</td><td>";
+            html += result[i][2];
+            html += "</td><td>";
+            html += result[i][3];
+            html += "</td><td>";
+            html += result[i][4];
+            html += "</td></tr>";
+        }
+        html += "</table>";
+        jQuery("#result").html(html);
+    }
+
+    function getUniversity(universityId, callback) {
+        jQuery.get({
+            url: "/id/eprint/" + universityId,
+            success: callback
+        });
+    }
+
     jQuery("#submit").click(function () {
         var value = jQuery("#institution-id").val();
         if (value.match(/^[0-9]+$/)) {
-            jQuery.get({
-                url: "/id/eprint/" + value,
-                success: function (result) {
-                    // XXX all of this is very naive
-                    var html = "<table>";
-                    for (var i = 0; i < result.length; ++i) {
-                        html += "<tr><td>";
-                        html += result[i][0];
-                        html += "</td><td>";
-                        html += result[i][1];
-                        html += "</td><td>";
-                        html += result[i][2];
-                        html += "</td><td>";
-                        html += result[i][3];
-                        html += "</td><td>";
-                        html += result[i][4];
-                        html += "</td></tr>";
-                    }
-                    html += "</table>";
-                    jQuery("#result").html(html);
-                }
-            });
+            getUniversity(value, drawTable);
         }
     });
 });
