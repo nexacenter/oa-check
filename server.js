56a21fec53e46142cd32b819102374694aa06de9
Simone Basso <bassosimone@gmail.com>
2016-04-14 22:12:32 +0200

    Revert "server.js: web api changes"
    This reverts commit d6ba88e504a262fcb2eb177557af0f5766119f4d.

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const availableApis = (_, res) => {
     res.json({
         "/api": "Return this list of APIs",
         "/api/": "Same as '/api'",
-        "/api/v1/institutions": "Scrape and return all roarmap institutions",
+        "/api/institutions": "Scrape and return all roarmap institutions",
         "/api/version": "Returns API version",
     });
 }
@@ -80,8 +80,8 @@ if (cluster.isMaster) {
 const app = express();
 app.get("/api", availableApis);
 app.get("/api/", availableApis);
-app.get("/api/v1/institutions", getInstitutions);
-app.get("/api/version", (_, res) => { res.json({version: 1}); });
+app.get("/api/institutions", getInstitutions);
+app.get("/api/version", (_, res) => { res.json({version: "0.0.2"}); });
 app.use(express.static(`${__dirname}/static`));
 app.listen(process.env.PORT || 8080, () => {
     console.log("web application started");
