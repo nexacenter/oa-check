3fa66347a35dc4283fb3ca6bb0122954d8b801ec
Simone Basso <bassosimone@gmail.com>
2016-04-17 13:15:23 +0200

    server: separate commands more clearly

--- server.js
+++ server.js
@@ -4,20 +4,7 @@
 "use strict";
 
 const cluster = require("cluster");
-const database = require("./lib/server/database");
-const express = require("express");
 const program = require("commander");
-const test = require("./lib/server/test");
-
-const getInstitutions = (callback) => {
-    database.query((err, data) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        callback(null, data);
-    });
-};
 
 program
     .version("0.0.4")
@@ -26,32 +13,14 @@ program
     .parse(process.argv);
 
 if (program.dump) {
-    database.query((err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log("Dump follows:\n\n");
-        console.log(data);
-    });
+    require("./lib/server/cmd_dump").main();
     return;
 }
 
 if (program.test) {
-    test.test();
+    require("./lib/server/cmd_test").main();
     return;
 }
-
-const availableApis = (_, res) => {
-    res.json({
-        "/api": "Return this list of APIs",
-        "/api/": "Same as '/api'",
-        "/api/v1/institutions": "Scrape and return all roarmap institutions",
-        "/api/version": "Returns API version",
-        "/id/eprint/[0-9]+": "Forwards entity query for ID to eprints",
-        "/cgi/search/simple": "Forwards search query to eprints",
-    });
-};
 
 // Robustness model: process requests using a child process and
 // respawn it in the event that it dies
@@ -65,39 +34,6 @@ if (cluster.isMaster) {
             cluster.fork();
         }, 15000);
     });
-    return;
+} else {
+    require("./lib/server/cmd_slave").main();
 }
-
-const app = express();
-app.get("/api", availableApis);
-app.get("/api/", availableApis);
-
-app.get("/api/v1/institutions", (_, res) => {
-    getInstitutions((err, d) => {
-        if (err) {
-            res.status(500).send("Internal server error");
-            return;
-        }
-        res.json(d);
-    });
-});
-
-app.get("/api/version", (_, res) => {
-    res.json({
-        version: 1
-    });
-});
-app.use(express.static(`${__dirname}/static`));
-
-// Before listening attempt once to fetch the institutions such that in
-// the common case we start serving when we have good institutions.
-// XXX This algorithm could of course become smarter than it is now...
-getInstitutions((error) => {
-    if (error) {
-        console.log("error when getting institutions:", error);
-        // fallthrough
-    }
-    app.listen(process.env.PORT || 8080, () => {
-        console.log("web application started");
-    });
-});
