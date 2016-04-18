5e5af705215bc5e103265ccca3321c97984b7866
Simone Basso <bassosimone@gmail.com>
2016-04-18 23:33:14 +0200

    js-beautify files in test/server/

--- test/server/regress.js
+++ test/server/regress.js
@@ -12,23 +12,23 @@ const scrape = require("../../lib/server/scrape");
 describe("the rules functionality", () => {
     it("does not introduce regressions", (done) => {
 
-    scrape.scrape((err, data) => {
-        if (err) {
-            throw err;
-        }
+        scrape.scrape((err, data) => {
+            if (err) {
+                throw err;
+            }
 
-        const novel = JSON.parse(data).map(policy => ({
-            [policy.eprintid]: rules.apply(policy).map(compliance => ({
-                [compliance.field_id]: compliance.is_compliant
-            })).reduce((all, one) => (Object.assign(all, one)))
-        })).reduce((all, one) => (Object.assign(all, one)));
+            const novel = JSON.parse(data).map(policy => ({
+                [policy.eprintid]: rules.apply(policy).map(compliance => ({
+                    [compliance.field_id]: compliance.is_compliant
+                })).reduce((all, one) => (Object.assign(all, one)))
+            })).reduce((all, one) => (Object.assign(all, one)));
 
-        const orig = JSON.parse(fs.readFileSync(filename));
+            const orig = JSON.parse(fs.readFileSync(filename));
 
-        const ok = equals(orig, novel);
-        console.log(ok);
-        done();
-    });
+            const ok = equals(orig, novel);
+            console.log(ok);
+            done();
+        });
 
-}).timeout(30000);
+    }).timeout(30000);
 });
