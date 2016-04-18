4aaca53b39e7ef815cb73582bec53e62ea53580b
Simone Basso <bassosimone@gmail.com>
2016-04-18 23:35:36 +0200

    js-beautify files in test/server/ (#52)

--- test/server/database.js
+++ test/server/database.js
@@ -10,35 +10,35 @@ describe("database", () => {
     describe("update_", () => {
         it("correctly updates the underlying data base", (done) => {
 
-let basicDocument = {
-    foo: 3.14,
-    foobar: [1, 2, 3],
-    bar: "baz",
-    jarjar: null
-};
+            let basicDocument = {
+                foo: 3.14,
+                foobar: [1, 2, 3],
+                bar: "baz",
+                jarjar: null
+            };
 
-database.update_((err, data) => {
-    assert.isNull(err);
-    assert.deepEqual(JSON.parse(data.val), basicDocument);
+            database.update_((err, data) => {
+                assert.isNull(err);
+                assert.deepEqual(JSON.parse(data.val), basicDocument);
 
-    database.update_((err, data) => {
-        assert.isNull(err);
-        const val = JSON.parse(data.val);
-        assert.strictEqual(val.foo, basicDocument.foo);
-        assert.deepEqual(val.foobar, [1, 2, 3, 4]);
-        assert.strictEqual(val.baz, basicDocument.baz);
-        assert.strictEqual(val.jarjar, basicDocument.jarjar);
-        done();
+                database.update_((err, data) => {
+                    assert.isNull(err);
+                    const val = JSON.parse(data.val);
+                    assert.strictEqual(val.foo, basicDocument.foo);
+                    assert.deepEqual(val.foobar, [1, 2, 3, 4]);
+                    assert.strictEqual(val.baz, basicDocument.baz);
+                    assert.strictEqual(val.jarjar, basicDocument.jarjar);
+                    done();
 
-    }, (callback) => {
-        basicDocument.foobar.push(4);
-        callback(null, JSON.stringify(basicDocument));
+                }, (callback) => {
+                    basicDocument.foobar.push(4);
+                    callback(null, JSON.stringify(basicDocument));
+                });
+
+            }, (callback) => {
+                callback(null, JSON.stringify(basicDocument));
+            });
+
+        });
     });
-
-}, (callback) => {
-    callback(null, JSON.stringify(basicDocument));
-});
-
-});
-});
 });
